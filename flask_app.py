31567633fa286ac33dccc60d26b945e8ecf1e683
bnbong <bbbong9@gmail.com>
2022-05-23 14:34:48 +0900

    fixing  and  testing  db  connection

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 # CORS(app)
 
 def get_db_connection():
