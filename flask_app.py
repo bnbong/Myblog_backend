02172e1124608bf96d1e178096247c30d379de8b
bnbong <bbbong9@gmail.com>
2022-05-23 04:53:46 +0000

    fixing db connection

--- flask_app.py
+++ flask_app.py
@@ -8,12 +8,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 # CORS(app)
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row()
+    conn.row_factory = sqlite3.Row
     return conn
 
 @app.route('/')
@@ -28,8 +28,9 @@ def index():
         note['content'] = markdown.markdown(note['content'])
         notes.append(note)
 
-    # index.html from Myblog_frontend 
-    return render_template('~/Myblog_frontend/templates/index.html', notes=notes)
+    # index.html from Myblog_frontend
+    # return (f"{note['id'], note['created']}")
+    return notes
 
 if __name__ == '__main__':
     app.run(debug=True)
