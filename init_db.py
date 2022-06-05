5f202bd3f49f914ea4983fdcb53dfbf28d64dda6
bnbong <bbbong9@gmail.com>
2022-06-05 21:00:45 +0900

    [MYB-19]Update  init_db.py

--- init_db.py
+++ init_db.py
@@ -7,38 +7,6 @@ from datetime import datetime
 import os
 import markdown
 
-delete_all_posts_from_DB()
-
-post_dir = os.path.abspath('../Myblog_posts/posts')
-category_name_lists = os.listdir(post_dir)
-
-# possible category lists:
-# IT, Study, Hobby, My_Daily_Life, Development, Game = \
-#     listdirname[0], listdirname[1], listdirname[2], listdirname[3], listdirname[4], listdirname[5]
-
-for category in category_name_lists:
-  post_dates = os.listdir(os.path.join(post_dir, category))
-
-  db_selected = Post.query.filter_by(tag=category).all()
-  db_dates = []
-  for post in db_selected:
-      db_dates.append(post.get_exact_created())
-
-  for date in post_dates:
-      year, month, day, hour, minute, sec = split_datestring_into_datetime(date)
-      new_date = datetime(year, month, day, hour, minute, sec)
-
-      # checking and add newest posts at selected categories
-      if str(new_date) not in db_dates:
-          post_created = new_date
-          post_title = get_title_from_txt_file()
-          post_thumbnail_url = get_thumbnail_url_from_txt_file()
-          post_content = get_markdowned_content_from_txt_file()
-
-          new_post = Post(title=post_title, thumbnail_url=post_thumbnail_url, content=post_content, \
-              content_preview=post_content_preview, created=post_created, tag=category)
-
-          db.session.add(new_post)
 
 def delete_all_posts_from_DB():
     posts = Post.query.all()
@@ -74,5 +42,39 @@ def get_markdowned_content_from_txt_file():
         f3.close()
 
     return post_content
+
+delete_all_posts_from_DB()
+
+post_dir = os.path.abspath('../Myblog_posts/posts')
+category_name_lists = os.listdir(post_dir)
+
+# possible category lists:
+# IT, Study, Hobby, My_Daily_Life, Development, Game = \
+#     listdirname[0], listdirname[1], listdirname[2], listdirname[3], listdirname[4], listdirname[5]
+
+for category in category_name_lists:
+  post_dates = os.listdir(os.path.join(post_dir, category))
+
+  db_selected = Post.query.filter_by(tag=category).all()
+  db_dates = []
+  for post in db_selected:
+      db_dates.append(post.get_exact_created())
+
+  for date in post_dates:
+      year, month, day, hour, minute, sec = split_datestring_into_datetime(date)
+      new_date = datetime(year, month, day, hour, minute, sec)
+
+      # checking and add newest posts at selected categories
+      if str(new_date) not in db_dates:
+          post_created = new_date
+          post_title = get_title_from_txt_file()
+          post_thumbnail_url = get_thumbnail_url_from_txt_file()
+          post_content = get_markdowned_content_from_txt_file()
+
+          new_post = Post(title=post_title, thumbnail_url=post_thumbnail_url, content=post_content, \
+              content_preview=post_content_preview, created=post_created, tag=category)
+
+          db.session.add(new_post)
+
 
 db.session.commit()
