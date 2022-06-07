8256ae32b14bfff4e8ce2ca1224d355c18e0b416
bnbong <bbbong9@gmail.com>
2022-06-07 17:25:01 +0900

    [MYB-19]update  update_db.py

--- update_db.py
+++ update_db.py
@@ -1,33 +1,60 @@
 # TODO: make fuctions which update existing posts when they updated. 
 # the post id cannot be modified, so find post using post_id and update it.
+from init_db import GetPost
 
-post_dir = os.path.abspath('../Myblog_posts/posts')
-category_name_lists = os.listdir(post_dir)
+class UpdatePost(GetPost):
+    
+    def __init__(self):
+        pass
 
-# possible category lists:
-# IT, Study, Hobby, My_Daily_Life, Development, Game = \
-#     listdirname[0], listdirname[1], listdirname[2], listdirname[3], listdirname[4], listdirname[5]
+    def update_all_posts_data(self):
+        posts = self.get_all_posts_at_db()
 
-for category in category_name_lists:
-    post_dates = os.listdir(os.path.join(post_dir, category))
+        '''
+        cases:
+            1. new post uploaded
+            2. post deleted
+            3. post updated (rename title or refactor created)
+        '''
+        for post in posts:
+            pass
 
-db_selected = Post.query.filter_by(tag=category).all()
-db_dates = []
-for post in db_selected:
-    db_dates.append(post.get_exact_created())
+    def get_all_posts_at_db(self):
+        posts = Post.query.all()
 
-for date in post_dates:
-    year, month, day, hour, minute, sec = split_datestring_into_datetime(date)
-    new_date = datetime(year, month, day, hour, minute, sec)
+        return posts
 
-    # checking and add newest posts at selected categories
-    if str(new_date) not in db_dates:
-        post_created, post_title,post_thumbnail_url, post_content, post_content_preview\
-            = get_new_post_data()
 
-        new_post = Post(title=post_title, thumbnail_url=post_thumbnail_url, content=post_content, \
-            content_preview=post_content_preview, created=post_created, tag=category)
+if __name__ == '__main__':
+    UpdatePost()
 
-        db.session.add(new_post)
+# post_dir = os.path.abspath('../Myblog_posts/posts')
+# category_name_lists = os.listdir(post_dir)
 
-db.session.commit()
+# # possible category lists:
+# # IT, Study, Hobby, My_Daily_Life, Development, Game = \
+# #     listdirname[0], listdirname[1], listdirname[2], listdirname[3], listdirname[4], listdirname[5]
+
+# for category in category_name_lists:
+#     post_dates = os.listdir(os.path.join(post_dir, category))
+
+# db_selected = Post.query.filter_by(tag=category).all()
+# db_dates = []
+# for post in db_selected:
+#     db_dates.append(post.get_exact_created())
+
+# for date in post_dates:
+#     year, month, day, hour, minute, sec = split_datestring_into_datetime(date)
+#     new_date = datetime(year, month, day, hour, minute, sec)
+
+#     # checking and add newest posts at selected categories
+#     if str(new_date) not in db_dates:
+#         post_created, post_title,post_thumbnail_url, post_content, post_content_preview\
+#             = get_new_post_data()
+
+#         new_post = Post(title=post_title, thumbnail_url=post_thumbnail_url, content=post_content, \
+#             content_preview=post_content_preview, created=post_created, tag=category)
+
+#         db.session.add(new_post)
+
+# db.session.commit()
